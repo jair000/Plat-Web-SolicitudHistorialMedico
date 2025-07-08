d34e1d5a743d407ac87b2caf47e30211314da249
Coco <102814124+jair000@users.noreply.github.com>
2025-07-08 00:57:50 -0500

    ya me llego al pinocho

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173
+  },
+  // 👇 Esto es esencial para routing SPA como /login, /perfil, etc.
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
